ea23b56a40e1ae5397aa58dc8b8f68eac2582ed9
lamtota40 <lamtota40@gmail.com>
2025-04-21 17:24:25 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ sudo apt install grml-rescueboot -y
 sudo wget -O /boot/grml download.grml.org/grml-full-2024.12-amd64.iso -O grml.iso
 sudo bash -c "echo 'CUSTOM_BOOTOPTIONS=\"ssh=pas123 vnc=pas123 dns=8.8.8.8,8.8.4.4 netscript=raw.githubusercontent.com/lamtota40/installwindows/main/autogrml.sh startx toram\"' >> /etc/default/grml-rescueboot"
 sudo update-grub
+sudo grub-reboot

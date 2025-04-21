80589819f289c2c3ae4f9e577c366de224109aee
lamtota40 <lamtota40@gmail.com>
2025-04-21 17:18:04 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ sudo hostnamectl set-hostname toxa.ix.tc
 sudo apt update
 
 sudo apt install grml-rescueboot -y
-sudo wget -O /boot/grml download.grml.org/grml64-full_2022.11.iso -O grml.iso
-sudo bash -c "echo 'CUSTOM_BOOTOPTIONS=\"ssh=pas123 vnc=pas123 dns=8.8.8.8,8.8.4.4 netscript=raw.githubusercontent.com/lamtota40/tes/main/setup-ngrok.sh startx toram\"' >> /etc/default/grml-rescueboot"
+sudo wget -O /boot/grml download.grml.org/grml-full-2024.12-amd64.iso -O grml.iso
+sudo bash -c "echo 'CUSTOM_BOOTOPTIONS=\"ssh=pas123 vnc=pas123 dns=8.8.8.8,8.8.4.4 netscript=raw.githubusercontent.com/lamtota40/installwindows/main/autogrml.sh startx toram\"' >> /etc/default/grml-rescueboot"
 sudo update-grub

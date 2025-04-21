e58a173b372fe01f09ac2d7f3ae7e359e0747025
lamtota40 <lamtota40@gmail.com>
2025-04-21 16:53:42 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+sudo apt install grml-rescueboot -y
+sudo wget -O /boot/grml download.grml.org/grml64-full_2022.11.iso -O grml.iso
+sudo bash -c "echo 'CUSTOM_BOOTOPTIONS=\"ssh=pas123 vnc=pas123 dns=8.8.8.8,8.8.4.4 netscript=raw.githubusercontent.com/lamtota40/tes/main/setup-ngrok.sh startx toram\"' >> /etc/default/grml-rescueboot"
